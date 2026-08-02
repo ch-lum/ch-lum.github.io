fa6a1654e8c9192bf6037f45e911e73ce3cb6e45
Chrissy Lum <chrlum03@gmail.com>
2026-08-02 11:37:23 -0700

    add buttons

--- src/lib/site.ts
+++ src/lib/site.ts
@@ -2,29 +2,29 @@ export const navigation = [
   {
     label: 'Projects',
     href: '/projects/',
-    image: '/home_imgs/projects-closed.svg',
-    hoverImage: '/home_imgs/projects-open.svg',
+    image: '/home_imgs/projects-closed.PNG',
+    hoverImage: '/home_imgs/projects-open.PNG',
     motion: 'swap'
   },
   {
     label: 'Long Form',
     href: '/long-form/',
-    image: '/home_imgs/long-form-closed.svg',
-    hoverImage: '/home_imgs/long-form-open.svg',
+    image: '/home_imgs/long-form-closed.PNG',
+    hoverImage: '/home_imgs/long-form-open.PNG',
     motion: 'swap'
   },
   {
     label: 'Short Form',
     href: '/short-form/',
-    image: '/home_imgs/short-form-closed.svg',
-    hoverImage: '/home_imgs/short-form-open.svg',
+    image: '/home_imgs/short-form-closed.PNG',
+    hoverImage: '/home_imgs/short-form-open.PNG',
     motion: 'swap'
   },
   {
     label: 'D&D',
     href: '/dnd/',
-    image: '/home_imgs/dnd-closed.svg',
-    hoverImage: '/home_imgs/dnd-open.svg',
+    image: '/home_imgs/dnd-closed.PNG',
+    hoverImage: '/home_imgs/dnd-open.PNG',
     motion: 'swap'
   },
   { label: 'coffee', href: '/coffee/', image: '/home_imgs/kettle.PNG', motion: 'tip' },
